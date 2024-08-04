a680a46b4b36236f3f70bbaa89da1c46f4f24718
Văn Thiết ( Frontend DEV ) <125172255+vanthiet1@users.noreply.github.com>
2024-08-04 10:38:55 +0700

    Update http.js

--- Client/src/utils/http.js
+++ Client/src/utils/http.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:1000/api/v1', 
+  baseURL: 'https://socical-chat.onrender.com/api/v1', 
+  // baseURL: 'http://localhost:1000/api/v1', 
   timeout: 10000, 
   headers: {
     'Content-Type': 'application/json',
