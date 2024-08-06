72e899deadf168756c19a9601516e6481841d02c
Văn Thiết ( Frontend DEV ) <125172255+vanthiet1@users.noreply.github.com>
2024-08-06 22:18:17 +0700

    Update socketConfig.js

--- Client/src/config/socketConfig.js
+++ Client/src/config/socketConfig.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
-const SOCKET_URL = 'http://localhost:1000';
-// const SOCKET_URL = 'https://socical-chat.onrender.com';
+// const SOCKET_URL = 'http://localhost:1000';
+const SOCKET_URL = 'https://socical-chat.onrender.com';
 
 const socket = io(SOCKET_URL, {
     transports: ['websocket'],
